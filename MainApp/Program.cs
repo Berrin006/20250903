4c8b4c6e8cdc2c9fb61a3a13557153dc6984205a
berrink <berrin.kizildagli@yh.nackademin.se>
2025-09-03 12:10:47 +0200

    input output example completed

--- MainApp/Program.cs
+++ MainApp/Program.cs
@@ -1,1 +1,34 @@
-﻿
+﻿List<string> name_list = [];
+
+Console.WriteLine("Hej och Välkommen: ");
+
+string answer = " ";
+
+do
+{
+    Console.Write("Ange ett namn: ");
+
+    string input_name = Console.ReadLine()!;
+
+    if (!string.IsNullOrEmpty(input_name))
+    {
+        name_list.Add(input_name);
+    }
+    Console.Clear();
+
+    Console.Write("Vill du ange ett nytt namn? (j/n): ");
+    answer = Console.ReadLine()!;
+}
+while (answer == "j");
+
+
+Console.Clear();
+
+Console.WriteLine("*** NAMNLISTA ***");
+
+foreach (string name in name_list)
+{
+    Console.WriteLine(name);
+}
+
+Console.ReadKey();
